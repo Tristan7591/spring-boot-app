9bca5e0851eaa2651021dfc3f4780c24bddd6792
tristan <tristan13bellonie@gmail.com>
2024-11-20 22:31:52 +0100

    mise a jour pour les metrics

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.MetricPublisher; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String helloWorld() {
-       
-        MetricPublisher.publishMetric("HelloWorldRequests", 1.0);
-
-       
         return "Hello World!";
     }
 }
